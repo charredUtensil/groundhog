8b495e9a9e8257017a48e3aaf51e62b0a0b696b7
charredUtensil <charredUtensil@users.noreply.github.com>
2024-03-09 15:41:35 -0500

    don't run prettier on the tile defs because the columns look better

--- src/core/models/tiles.ts
+++ src/core/models/tiles.ts
@@ -1,5 +1,3 @@
-/* eslint-disable prettier/prettier */
-
 class _Tile<ID> {
   readonly id: ID;
   readonly isWall: boolean;
@@ -27,28 +25,29 @@ class _Tile<ID> {
 // Where possible, use colors from
 // https://github.com/trigger-segfault/legorockraiders-analysis/blob/main/docs/LegoRR_Colors.h
 
+// prettier-ignore
 export const Tile = {
-  FLOOR: new _Tile<1>(1, false, true, 0, 0, "#2D004B"),
-  LAVA: new _Tile<6>(6, false, false, 0, 0, "#FF5A00"),
-  WATER: new _Tile<11>(11, false, false, 0, 0, "#002FB5"),
-  DIRT: new _Tile<26>(26, true, true, 0, 4, "#AD59EF"),
-  LOOSE_ROCK: new _Tile<30>(30, true, true, 0, 4, "#943CC3"),
-  HARD_ROCK: new _Tile<34>(34, true, true, 0, 4, "#731CAD"),
-  SOLID_ROCK: new _Tile<38>(38, true, false, 0, 0, "#800080"),
-  RUBBLE_1: new _Tile<2>(2, false, true, 0, 1, "#180032"),
-  RUBBLE_2: new _Tile<3>(3, false, true, 0, 2, "#180032"),
-  RUBBLE_3: new _Tile<4>(4, false, true, 0, 3, "#180032"),
-  RUBBLE_4: new _Tile<5>(5, false, true, 0, 4, "#180032"),
-  SLUG_HOLE: new _Tile<12>(12, false, true, 0, 0, "#BD2020"),
-  FOUNDATION: new _Tile<14>(14, false, true, 0, 0, "#BFBFBF"),
-  POWER_PATH: new _Tile<24>(24, false, true, 0, 0, "#9D9B00"),
-  LANDSLIDE_RUBBLE_4: new _Tile<60>(60, false, true, 0, 0, "#180032"),
-  LANDSLIDE_RUBBLE_3: new _Tile<61>(61, false, true, 0, 0, "#180032"),
-  LANDSLIDE_RUBBLE_2: new _Tile<62>(62, false, true, 0, 0, "#180032"),
-  LANDSLIDE_RUBBLE_1: new _Tile<63>(63, false, true, 0, 0, "#180032"),
-  CRYSTAL_SEAM: new _Tile<42>(42, true, true, 4, 4, "#B5FF00"),
-  ORE_SEAM: new _Tile<46>(46, true, true, 0, 8, "#9C4108"),
-  RECHARGE_SEAM: new _Tile<50>(50, true, false, 0, 0, "#FFFF00"),
+  FLOOR:              new _Tile< 1>( 1, false,  true, 0, 0, "#2D004B"),
+  LAVA:               new _Tile< 6>( 6, false, false, 0, 0, "#FF5A00"),
+  WATER:              new _Tile<11>(11, false, false, 0, 0, "#002FB5"),
+  DIRT:               new _Tile<26>(26,  true,  true, 0, 4, "#AD59EF"),
+  LOOSE_ROCK:         new _Tile<30>(30,  true,  true, 0, 4, "#943CC3"),
+  HARD_ROCK:          new _Tile<34>(34,  true,  true, 0, 4, "#731CAD"),
+  SOLID_ROCK:         new _Tile<38>(38,  true, false, 0, 0, "#800080"),
+  RUBBLE_1:           new _Tile< 2>( 2, false,  true, 0, 1, "#180032"),
+  RUBBLE_2:           new _Tile< 3>( 3, false,  true, 0, 2, "#180032"),
+  RUBBLE_3:           new _Tile< 4>( 4, false,  true, 0, 3, "#180032"),
+  RUBBLE_4:           new _Tile< 5>( 5, false,  true, 0, 4, "#180032"),
+  SLUG_HOLE:          new _Tile<12>(12, false,  true, 0, 0, "#BD2020"),
+  FOUNDATION:         new _Tile<14>(14, false,  true, 0, 0, "#BFBFBF"),
+  POWER_PATH:         new _Tile<24>(24, false,  true, 0, 0, "#9D9B00"),
+  LANDSLIDE_RUBBLE_4: new _Tile<60>(60, false,  true, 0, 0, "#180032"),
+  LANDSLIDE_RUBBLE_3: new _Tile<61>(61, false,  true, 0, 0, "#180032"),
+  LANDSLIDE_RUBBLE_2: new _Tile<62>(62, false,  true, 0, 0, "#180032"),
+  LANDSLIDE_RUBBLE_1: new _Tile<63>(63, false,  true, 0, 0, "#180032"),
+  CRYSTAL_SEAM:       new _Tile<42>(42,  true,  true, 4, 4, "#B5FF00"),
+  ORE_SEAM:           new _Tile<46>(46,  true,  true, 0, 8, "#9C4108"),
+  RECHARGE_SEAM:      new _Tile<50>(50,  true, false, 0, 0, "#FFFF00"),
 };
 
 export type Tile = _Tile<any>;
