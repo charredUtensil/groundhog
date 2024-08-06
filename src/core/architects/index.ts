fae393e713df5abbf3726b458bf8bb771230edee
charredUtensil <charredUtensil@users.noreply.github.com>
2024-08-05 23:39:36 -0400

    re-refactor metadata

--- src/core/architects/index.ts
+++ src/core/architects/index.ts
@@ -1,14 +1,21 @@
 import { Architect } from "../models/architect";
-import { ESTABLISHED_HQ } from "./established_hq";
+import { ESTABLISHED_HQ, HqMetadata } from "./established_hq";
 import FLOODED from "./flooded";
-import LOST_MINERS from "./lost_miners";
-import NOMAD_SPAWN from "./nomads";
+import LOST_MINERS, { LostMinersMetadata } from "./lost_miners";
+import NOMAD_SPAWN, { NomadsMetadata } from "./nomads";
 import SIMPLE_CAVE from "./simple_cave";
 import SIMPLE_HALL from "./simple_hall";
 import SIMPLE_SPAWN from "./simple_spawn";
 import SLUGS from "./slugs";
 import THIN_HALL from "./thin_hall";
 import TREASURE from "./treasure";
+
+export type AnyMetadata =
+  | undefined
+  | HqMetadata
+  | LostMinersMetadata
+  | NomadsMetadata
+  | { tag: "slugNest" | "treasure" };
 
 export const ARCHITECTS = [
   ...ESTABLISHED_HQ,
@@ -21,6 +28,4 @@ export const ARCHITECTS = [
   ...SLUGS,
   ...THIN_HALL,
   ...TREASURE,
-] as const satisfies readonly Architect<any>[];
-
-export type AnyMetadata = ReturnType<(typeof ARCHITECTS)[number]["prime"]>;
+] as const satisfies readonly Architect<AnyMetadata>[];
