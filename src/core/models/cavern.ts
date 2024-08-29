cf039d1ac86494980b99406799653e286505961f
charredUtensil <charredUtensil@users.noreply.github.com>
2024-08-28 23:26:25 -0400

    make code work again

--- src/core/models/cavern.ts
+++ src/core/models/cavern.ts
@@ -4,14 +4,12 @@ import { CavernContext } from "../common/context";
 import { AnyTfResultOf } from "../common/transform";
 import { CollapseUnion } from "../common/utils";
 import { CAVERN_TF } from "../transformers";
-import { Plan } from "./plan";
+import { PearledCavern } from "../transformers/01_planning/06_pearl";
 
 export type BaseCavern = {
   context: CavernContext;
   dice: DiceBox;
 };
-export type PlannedCavern = BaseCavern & {
-  plans: readonly Plan<AnyMetadata>[];
-};
+export type PlannedCavern = PearledCavern;
 
 export type Cavern = CollapseUnion<AnyTfResultOf<typeof CAVERN_TF>>;
