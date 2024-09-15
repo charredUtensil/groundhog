214560b16cb9e5f16440216d7f8de1bd562bc2f3
charredUtensil <charredUtensil@users.noreply.github.com>
2024-09-15 16:48:26 -0400

    it works

--- bin/groundhog-gen.ts
+++ bin/groundhog-gen.ts
@@ -1,16 +1,15 @@
 #!/usr/bin/env node
-import { DiceBox, inferContextDefaults } from "../src/core/common";
+import { inferContextDefaults } from "../src/core/common";
 import { CAVERN_TF } from "../src/core/transformers";
 
 function generate() {
   const seed = 0xEFE63E54;
   let state = CAVERN_TF.first({
-    context: inferContextDefaults({seed}),
-    dice: new DiceBox(seed),
+    initialContext: inferContextDefaults({seed}),
   });
   
   while (state.next) {
-    console.log(`${state.progress} ${state.name}`);
+    console.log(`${(state.progress * 100).toFixed()}% ${state.name}`);
     state = state.next();
   }
 }
