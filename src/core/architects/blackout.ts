8d708bbe034850a542f53257dcb3f2aa328b89f9
charredUtensil <charredUtensil@users.noreply.github.com>
2024-10-29 10:10:19 -0400

    update blackout

--- src/core/architects/blackout.ts
+++ src/core/architects/blackout.ts
@@ -4,12 +4,12 @@ import { DefaultSpawnArchitect, PartialArchitect } from "./default";
 import { mkRough, Rough } from "./utils/rough";
 import { eventChain, mkVars, scriptFragment } from "./utils/script";
 
-
-const CRYSTALS = {
-  BASE: 10,
-  RESET_INCREMENT: 7,
-  INCREMENT: 15,
-} as const;
+const CRYSTALS_INITIAL = 10;
+const CRYSTALS_INCREMENT_TO_RESET = 7;
+const CRYSTALS_INCREMENT_TO_RETRIGGER = 15;
+const MIN_AIR = 500;
+const RESET_SECONDS = 120;
+const RESET_END = 999;
 
 const BASE: PartialArchitect<undefined> = {
   ...DefaultSpawnArchitect,
@@ -29,7 +29,7 @@ const BASE: PartialArchitect<undefined> = {
     const rng = cavern.dice.script(plan.id);
     return scriptFragment(
       `# P${plan.id}: Blackout`,
-      sh.declareInt(v.needCrystals, CRYSTALS.BASE),
+      sh.declareInt(v.needCrystals, CRYSTALS_INITIAL),
       sh.declareInt(v.crystalBank, 0),
       sh.declareInt(v.tc, 0),
       sh.declareInt(v.trips, 0),
@@ -47,7 +47,7 @@ const BASE: PartialArchitect<undefined> = {
       sh.trigger(
         `when(${v.trips}==1)`,
         'wait:random(5)(30);',
-        `${v.needCrystals}=crystals+${CRYSTALS.RESET_INCREMENT};`,
+        `${v.needCrystals}=crystals+${CRYSTALS_INCREMENT_TO_RESET};`,
         'disable:lights;',
         `${v.reset}=0;`,
         `${v.loop};`,
@@ -55,23 +55,23 @@ const BASE: PartialArchitect<undefined> = {
         `${v.ms}=1;`,
       ),
       `if(${v.ms}==1)[msg:${v.msgStart}]`,
-      `when(building.dead)[${v.reset}=999]`,
-      `when(vehicle.dead)[${v.reset}=999]`,
-      `when(${v.crystalBank}>=${v.needCrystals})[${v.reset}=999]`,
+      `when(building.dead)[${v.reset}=${RESET_END}]`,
+      `when(vehicle.dead)[${v.reset}=${RESET_END}]`,
+      `when(${v.crystalBank}>=${v.needCrystals})[${v.reset}=${RESET_END}]`,
       eventChain(
         v.loop,
         `${v.tc}=crystals;`,
         `crystals-=${v.tc};`,
         `${v.crystalBank}+=${v.tc};`,
         `wait:1;`,
-        cavern.oxygen && `((air<500))${v.reset}=999;`,
+        cavern.oxygen && `((air<${MIN_AIR}))${v.reset}=${RESET_END};`,
         `${v.reset}+=1;`,
-        `((${v.reset}>120))[${v.doReset}][${v.loop}];`,
+        `((${v.reset}>${RESET_SECONDS}))[${v.doReset}][${v.loop}];`,
       ),
       eventChain(
         v.doReset,
         `crystals+=${v.crystalBank};`,
-        `${v.needCrystals}=${v.crystalBank}+${CRYSTALS.INCREMENT};`,
+        `${v.needCrystals}=${v.crystalBank}+${CRYSTALS_INCREMENT_TO_RETRIGGER};`,
         `${v.crystalBank}=0;`,
         'wait:1;',
         'enable:lights;',
@@ -93,8 +93,12 @@ const BLACKOUT = [
       { of: Rough.LOOSE_ROCK, grow: 1 },
       { of: Rough.MIX_FRINGE },
     ),
-    anchorBid: ({ plan }) =>
-      !plan.fluid && plan.lakeSize >= 3 && plan.pearlRadius > 0 && 1,
+    anchorBid: ({ cavern, plan }) =>
+      !plan.fluid &&
+      plan.lakeSize >= 3 &&
+      plan.pearlRadius > 0 &&
+      !cavern.context.hasSlugs &&
+      0.4,
   },
 ] as const satisfies readonly Architect<undefined>[];
 export default BLACKOUT;
