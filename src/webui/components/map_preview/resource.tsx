80ab19157a779eac3ed5c291ce4c084a4437be1f
charredUtensil <charredUtensil@users.noreply.github.com>
2025-05-04 18:11:51 -0400

    tweaks

--- src/webui/components/map_preview/resource.tsx
+++ src/webui/components/map_preview/resource.tsx
@@ -1,42 +1,45 @@
 import React from "react";
 import { Grid } from "../../../core/common/grid";
 import styles from "./style.module.scss";
+import { DiscoveryZone } from "../../../core/models/discovery_zone";
 
 const SCALE = 6;
 
 export default function ResourcePreview({
   ore,
   crystals,
+  discoveryZones,
 }: {
   ore?: Grid<number>;
   crystals?: Grid<number>;
+  discoveryZones?: Grid<DiscoveryZone>,
 }) {
+  function dot(offset: number, n:number, x:number, y:number) {
+    if (!discoveryZones?.get(x, y)?.openOnSpawn) {
+      return null;
+    }
+    return new Array(n).fill(0).map((_, i) => {
+      return (
+        <circle
+          className={styles.ring}
+          cx={(x + (offset + 0.2 + (y + i * i) * 0.53) % 1) * SCALE}
+          cy={(y + (offset + 0.7 + (x + i * i) * 0.35) % 1) * SCALE}
+          r={0.5} />
+      );
+    })
+  }
   return (
-    <>
+    <g className={styles.resources}>
       {ore && (
         <g className={styles.ore}>
-          {ore.map((n, x, y) => (
-            <circle
-              className={styles.ring}
-              cx={(x + 0.25) * SCALE}
-              cy={(y + 0.25) * SCALE}
-              r={(n * SCALE) / 4}
-            />
-          ))}
+          {ore.map((...args) => dot(0.5, ...args))}
         </g>
       )}
       {crystals && (
         <g className={styles.crystals}>
-          {crystals.map((n, x, y) => (
-            <circle
-              className={styles.ring}
-              cx={(x + 0.75) * SCALE}
-              cy={(y + 0.75) * SCALE}
-              r={(n * SCALE) / 4}
-            />
-          ))}
+          {crystals.map((...args) => dot(0, ...args))}
         </g>
       )}
-    </>
+    </g>
   );
 }
